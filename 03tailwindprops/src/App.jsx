d2c74b5f093a337259c291f3487f57fe84aa58e3
Vikash Katiyar <rishabktr018@gmail.com>
2024-04-14 13:16:21 +0530

    bgChanger, passwordgenerator complete project

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import Card from './assets/Card'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,9 +12,11 @@ function App() {
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
 
-      <Card channel="chaiaurcode"/>
+      <Card username="chaiaurcode" btnText="click me"/>
       <br/>
-      <Card channel="" myArr=[1,2,3]/>
+      <Card username="Vikash" btnText="visit me"/>
+      <br/>
+      <Card username="Rishbah" />
           
     </>
   )
